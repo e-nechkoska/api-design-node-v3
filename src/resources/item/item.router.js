a47abe3b51b3a568614a6236c5c2a49f500e93f9
e-nechkoska <e.nechkoska@gmail.com>
2020-05-20 15:38:37 +0200

    do chores for lesson-2

--- src/resources/item/item.router.js
+++ src/resources/item/item.router.js
@@ -2,4 +2,18 @@ import { Router } from 'express'
 
 const router = Router()
 
+const controller = (req, res) => {
+    res.send({message: 'hello'})
+}
+// /api/item
+router.route('/')
+    .get(controller)
+    .post(controller)
+
+// /api/item/:id
+router.route('/:id')
+    .get(controller)
+    .put(controller)
+    .delete(controller)   
+
 export default router
