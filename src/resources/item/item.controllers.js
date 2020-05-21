a66ccc9ca6de11b66ea6f2de68c96432f4c65835
e-nechkoska <e.nechkoska@gmail.com>
2020-05-21 14:45:09 +0200

    solution to exercise3 and creating item from model

--- src/resources/item/item.controllers.js
+++ src/resources/item/item.controllers.js
@@ -1,4 +1,24 @@
 import { crudControllers } from '../../utils/crud'
 import { Item } from './item.model'
+import mongoose from 'mongoose'
+import { connect } from '../../utils/db'
+import { format } from 'morgan'
 
-export default crudControllers(Item)
+const run = async () => {
+    await connect('mongodb://localhost:27017/api-test');
+
+    const item = Item.create({
+        name: 'Clean up',
+        createdBy: mongoose.Types.ObjectId(),
+        list: mongoose.Types.ObjectId()
+    })
+    
+    
+    item.then(() => {
+        mongoose.disconnect();
+        console.log(item);
+    });
+}
+
+run();
+// export default crudControllers(Item)
--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,4 +1,41 @@
-import mongoose from 'mongoose'
+import mongoose, { mongo } from 'mongoose'
 
-const itemSchema = new mongoose.Schema({}, { timestamps: true })
+const itemSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        maxlength: 50,
+        trim: true
+    },
+
+    status: {
+        type: String,
+        required: true,
+        default: 'active',
+        enum: [
+            'active',
+            'complete',
+            'pastdue'
+        ]
+    },
+
+    notes: String,
+
+    due: Date,
+
+    createdBy: {
+        ref: 'user',
+        required: true,
+        type: mongoose.SchemaTypes.ObjectId
+    },
+
+    list: {
+        type: mongoose.SchemaTypes.ObjectId,
+        ref: 'list',
+        required: true
+    }
+
+}, { timestamps: true })
+
+itemSchema.index({list: 1, name: 1}, {unique: true})
 export const Item = mongoose.model('item', itemSchema)
